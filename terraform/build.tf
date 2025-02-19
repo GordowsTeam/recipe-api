fb7549236e8383727e2ce8dde4aaddddaa4fc519
agVillalobos <agarciavillalobos87@gmail.com>
2025-02-19 18:46:03 -0500

    support ubuntu

--- terraform/build.tf
+++ terraform/build.tf
@@ -1,10 +1,15 @@
+locals {
+  # Directories start with "C:..." on Windows; All other OSs use "/" for root.
+  is_windows = substr(pathexpand("~"), 0, 1) == "/" ? false : true
+}
+
 resource "null_resource" "build_dotnet_lambda" {
   provisioner "local-exec" {
     command     = <<EOT
       dotnet restore ../src/RecipeAPI/RecipeAPI.csproj
       dotnet publish ../src/RecipeAPI/RecipeAPI.csproj -c Release -r linux-x64 --self-contained false -o ../RecipeAPI/publish
     EOT
-    interpreter = ["PowerShell", "-Command"]
+    interpreter = local.is_windows ? ["PowerShell", "-Command"] : []
   }
   triggers = {
     always_run = "${timestamp()}"
