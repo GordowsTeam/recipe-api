95029b1f475c12db867f31cce628c05734d563a1
agVillalobos <agarciavillalobos87@gmail.com>
2025-02-25 13:44:50 -0500

    test rulset (#3)

--- src/Recipe.Infrastructure/Services/RecipeRepository.cs
+++ src/Recipe.Infrastructure/Services/RecipeRepository.cs
@@ -9,7 +9,7 @@ public class RecipeRepository : IRecipeRepository
     {
         var recipes = new List<RecipeResponse>() { new ()
         {
-            Name = "Pasta 4",
+            Name = "Pasta 5",
             Images = new List<Image>() { new() { Url = "https://www.example.com/pasta.jpg", Main = true } },
             Ingredients = new List<Ingredient>() { new() { Text = "Pasta", Quantity = "1", Measure = "cup", Weight = 100, Food = "Pasta", FoodCategory = FoodCategory.None, FoodCategoryId = "1", Image = "https://www.example.com/pasta.jpg" } },
             MissingIngredients = new List<string>() { "Tomato", "Cheese" },
