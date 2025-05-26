d79bd86405203aa518ebfd57909ba643c448f6e6
gordows <Gordowsteams@gmail.com>
2025-05-26 18:54:58 -0500

    Develop (#29)
    * Feature/recipe detail (#20)
    
    * added recipe details
    
    * refactor
    
    * addede get recipe endpoint
    
    * added spoonacular api
    
    * implemented the recipe detail from spoonacular
    
    * clean up
    
    * added spoonacular config
    
    * changes for setting env dynamically
    
    ---------
    
    Co-authored-by: Alberto-Garcia-Villa <129812848+Alberto-Garcia-Villa@users.noreply.github.com>
    Co-authored-by: agVillalobos <agarciavillalobos87@gmail.com>
    
    * Update deploy.yml (#23)
    
    * Feature/enviornment creations (#26)
    
    * Update deploy.yml
    
    * Update deploy.yaml
    
    * update
    
    ---------
    
    Co-authored-by: gordows <Gordowsteams@gmail.com>
    
    ---------
    
    Co-authored-by: Alberto-Garcia-Villa <129812848+Alberto-Garcia-Villa@users.noreply.github.com>
    Co-authored-by: agVillalobos <agarciavillalobos87@gmail.com>

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "recipe-terraform-s3-state"       # Replace with your S3 bucket name
+    bucket = "recipe-terraform-s3-state-dev"       # Replace with your S3 bucket name
     key    = "terraform.tfstate"  # Path to store the tfstate in the S3 bucket
     region = "us-east-1"                  # Replace with your desired AWS region
     encrypt = true                        # Enable encryption at rest
-    dynamodb_table = "recipe-terraform-state-lock-dynamo"  # Table for state locking
+    dynamodb_table = "recipe-terraform-state-lock-dynamo-dev"  # Table for state locking
     acl    = "bucket-owner-full-control"  # Ensure proper permissions
   }
 }
